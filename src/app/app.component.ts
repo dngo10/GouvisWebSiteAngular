d94a57e8f15fb249a159a271ea3bc898c85b460a
Dao Ngo <dngo10@outlook.com>
2019-07-29 01:38:55 -0700

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {fromEvent} from 'rxjs/observable/fromEvent';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+
+  resizeObservable$: Observable<Event>;
+  resizeSubscription: Subscription;
+
+  ngOnInit(): void {
+    this.changeSize();
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.resizeObservable$ = fromEvent(window, 'resize');
+    this.resizeSubscription = this.resizeObservable$.subscribe(size => this.changeSize());
+  }
+
   title = 'GouvisWebsite';
+
+  firstClass = {
+    width: "100vw"
+  }
+
+  ngOnDestroy():void{
+    if(this.resizeSubscription){
+      this.resizeSubscription.unsubscribe();
+    }
+  }
+
+
+  changeSize(){
+    let windowWidth:number = window.innerWidth;
+    if(windowWidth >= 900){
+      this.firstClass={
+        width: "100vw"
+      }
+    } else{
+        this.firstClass={
+          width: "100vw"
+        }    
+      }
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { QuoteBlockComponent } from './components/quote-block/quote-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    QuoteBlockComponent
   ],
   imports: [
     BrowserModule,

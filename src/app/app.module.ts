536464b241d97c1803fb56b717a67ce14c6b59cf
Dao Ngo <dngo10@outlook.com>
2019-07-29 04:27:01 -0700

    Fix some

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { QuoteBlockComponent } from './components/quote-block/quote-block.component';
+import { GridImagesComponent } from './components/grid-images/grid-images.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    QuoteBlockComponent
+    QuoteBlockComponent,
+    GridImagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/grid-images/grid-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-images',
+  templateUrl: './grid-images.component.html',
+  styleUrls: ['./grid-images.component.css']
+})
+export class GridImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

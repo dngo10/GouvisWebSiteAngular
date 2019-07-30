569f5c07b21da4759ce3139743ee6f389ff9ee53
Dan Ngo <dngo10@outlook.com>
2019-07-29 17:12:00 -0700

    Done Home page

--- src/app/components/logo-home-page/logo-home-page.component.ts
+++ src/app/components/logo-home-page/logo-home-page.component.ts
@@ -12,4 +12,14 @@ export class LogoHomePageComponent implements OnInit {
   ngOnInit() {
   }
 
+  menuClick(){
+    document.getElementById("mySidenav").style.width = "0%";
+    document.getElementById("mySidenav").style.borderLeft = "#afb9b2 0vw solid";
+  }
+
+  menuClickOpen(){
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.borderLeft = "#afb9b2 0.5vw solid";
+  }
+
 }

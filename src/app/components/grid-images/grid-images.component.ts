e44f65404825e4e8ad355aef0f179a354d49e336
Dan Ngo <dngo10@outlook.com>
2019-07-31 11:35:46 -0700

    Add Animation To homePage--Image Grid

--- src/app/components/grid-images/grid-images.component.ts
+++ src/app/components/grid-images/grid-images.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-grid-images',
@@ -10,6 +11,7 @@ export class GridImagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    AOS.init();
   }
 
 }

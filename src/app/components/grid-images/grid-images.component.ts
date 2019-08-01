bfd16861848eb0540690d462dfa5902c4268b9f5
Dan Ngo <dngo10@outlook.com>
2019-07-31 17:58:50 -0700

    Add New Font

--- src/app/components/grid-images/grid-images.component.ts
+++ src/app/components/grid-images/grid-images.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-grid-images',
   templateUrl: './grid-images.component.html',
-  styleUrls: ['./grid-images.component.css']
+  styleUrls: ['./grid-images.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class GridImagesComponent implements OnInit {
 
+  offset: number =1000; //this equals to height of viewport
+  offsetString : string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
     AOS.init();
+  }
+
+
+
+  onResize(event){
+    this.offset = window.innerHeight;
+    this.offsetString = "data-aos=\"zoom-in\" data-aos-delay=\"" + this.offset + "\"";
+    AOS.refresh();
   }
 
 }
